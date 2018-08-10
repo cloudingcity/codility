5b5146983535859d8ef987171c67ab7b1f7a6476
clouding <ghost0436@gmail.com>
2018-08-10 19:28:24 +0800

    Add BinaryGap

---
+++ tests/BinaryGapTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Clouding\Codility\BinaryGap;
+
+class BinaryGapTest extends TestCase
+{
+    public function test_1041()
+    {
+        $N = 1041;
+
+        $this->assertSame(BinaryGap::solution($N), 5);
+    }
+
+    public function test_15()
+    {
+        $N = 15;
+
+        $this->assertSame(BinaryGap::solution($N), 0);
+    }
+
+    public function test_32()
+    {
+        $N = 32;
+
+        $this->assertSame(BinaryGap::solution($N), 0);
+    }
+}
